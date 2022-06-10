d45667c39dad503cc7a506c5f42401494f922c07
peterclermy <peterclermy232@gmail.com>
2022-06-10 08:58:42 +0300

    commit tree 0f4db198a99b11bdda917751a3679c1f7d09ebaf

---
+++ .history/index_20220610085616.js
@@ -0,0 +1,27 @@
+const farmAnimals = 'cow horse sheep pig chicken';
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const muppet = {
+  muppetName: 'Miss Piggy',
+  color: 'pink',
+  song: 'Never Before, Never Again',
+  job: 'Cast member of The Muppet Show',
+  partner: 'Kermit'
+};
+
+const nestedMuppet = {
+  nestedName: 'Kermit',
+  nestedColor: 'green',
+  album: {
+    theMuppetMovie: {
+      song1: 'Rainbow Connection',
+      song2: 'Moving Right Along',
+      song3: 'Never Before, Never Again',
+      song4: 'I Hope That Something Better Comes Along',
+    },
+  },
+  nestedJob: 'Host of The Muppet Show',
+  nestedPartner: 'Miss Piggy'
+};
+
